fc5c4df529f8115cb8a161daabdacbd4fabb0b87
Edmund <edmundjet99@gmail.com>
2021-10-31 22:38:14 -0700

    Firebase connection

---
+++ Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="home">
+    <ul class="collection with-header">
+      <li class="collection-header"><h4>Registered Users</h4></li>
+      <li v-for="user in users" v-bind:key="user.id" class="collection-item">
+        <div class="chip">{{user.dept}}</div>
+        {{user.employee_id}}: {{user.name}} 
+         <router-link class="secondary-content" v-bind:to="{ name: 'view-employee', params: { employee_id: user.employee_id }}"><i class="fa fa-eye"></i></router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import db from './firebaseInit'
+  export default {
+    name: 'home',
+    data () {
+      return {
+        users: [],
+        loading: true
+      }
+    },
+    created () {
+      db.collection('users').orderBy('dept').get().then((querySnapshot) => {
+        this.loading = false
+        querySnapshot.forEach((doc) => {
+          const data = {
+            'id': doc.id,
+            'employee_id': doc.data().employee_id,
+            'name': doc.data().name,
+            'dept': doc.data().dept,
+            'position': doc.data().position
+          }
+          this.users.push(data)
+        })
+      })
+    }
+  }
+</script>
