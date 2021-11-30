9f7a21b605d51bc5116217eb8bbd049292091ace
gdub21 <gdubbb21@mail.fresnostate.edu>
2021-11-29 22:05:39 -0800

    latest changes

--- Quotidian/src/components/Followlist.vue
+++ Quotidian/src/components/Followlist.vue
@@ -10,36 +10,37 @@
                 <input type="text" id="usernames" v-model="usernames" />
                 <label class="white-text" for="email">Enter username</label>
               </div>
-                <button
-                v-on:click="followlist"
-                class="btn btn-large btn-extended grey lighten-4 black-text"
-              >
-                Search
-              </button>
-              </div>
+                <button v-on:click="followlist"
+                class="btn btn-large btn-extended grey lighten-4 black-text">Search</button>
+                 <div v-for="user in users" v-bind:key="user.id" class="collection-item">
+                  <div class="text">{{user.name}}</div>
+                     <!-- <div id="options" v-if="seeoptions">-->
+                      <button v-on:click="friendrequest" class ="btn btn-large btn-extended grey lighten-4 black-text">Follow</button>
+                      <button v-on:click="friendrequest" class ="btn btn-large btn-extended grey lighten-4 black-text">View Profile</button>
+                 </div>
+         </div>
     </div>
-      <div class="list right red">
-        {{ word }}
-        <li v-for="user in users" v-bind:key="user.id" class="collection-item">
-
+      </div>
+      
+       <!-- <li v-for="user in users" v-bind:key="user.id" class="collection-item">
+          <div class="chip">{{user.name}}</div>
+          {{user.bio}}
         <button v-on:click="followlist">{{user.name}}</button>
-        <!--<button v-on:click="seeoptions = !seeoptions">{{user.name}}</button>-->
+        <!--<button v-on:click="seeoptions = !seeoptions">{{user.name}}</button>
         <div id="options" v-if="seeoptions">
         <li><button v-on:click="friendrequest" class ="btn btn-large btn-extended grey lighten-4 black-text">Follow</button></li>
         <li><button>Block</button></li>  
         <li><button>View Profile</button></li>
-        </div> 
+        
       </li>
       </div>
-     </div>
+     </div>-->
 </template>
-
 <style>
 template{
   background-color: blueviolet
 }
 </style>
-
 <script>
 //lets see if we can implement the search function using login as
 // a template, change data to match with what u need
@@ -55,12 +56,16 @@ export default {
       users: [],
       loading: true,
       usernames: "",
-      word: 'This is a friends list',
       content: 'Info of User',
       content2: 'A search bar',
-      //seeoptions1: false,
-     // seeoptions2: false,
-      //seeoptions3: false
+    }
+  },
+  name: "friendrequest",
+  data: function (){
+    return {
+      users: [],
+      loading: true,
+      //usernames: "",
     }
   },
   methods:{
@@ -70,32 +75,41 @@ export default {
         //console.log("succes");
        //checks through database for each documented ID username.
         querySnapshot.forEach((doc) => {
-        
+
            var curruser = this.username;
           if(this.usernames.trim() == doc.data().username.trim() ){
             console.log("SUCCESS")
                         console.log(doc.data().username)  
-                         var userToAdd;
-            const data = {
-                        
+                         var userToAddID = doc.data().username;
+                         console.log(userToAddID);
+                const data = {
                       'name': doc.data().username,
-                       
-            }//console.log(newFriendID)
+                      'details': doc.data().bio,
+              }
+                        this.users.push(data)
+
           }
           else
-            console.log("dont know")
-          
-  
+            console.log("none")
 
         })
-       // console.log(stringName)
+      
       })
+    },
+    friendrequest: function(e){
+      
+        var userID = auth.currentUser.uid;
+      //  const currFriendsList = [db.collection('users').doc(userID).data().friendslist]
+      //  currFriendsList.push(this.usernames)
+        db.collection('users').doc(userID).update({
+          friendslist: firebase.firestore.FieldValue.arrayUnion(this.usernames)
+        })
+
     },
    
   },
   created () {
-   // Boolean userExists = false;
-      
+   
       db.collection('users').orderBy('dept').get().then((querySnapshot) => {
         this.loading = false
         querySnapshot.forEach((doc) => {
@@ -103,7 +117,7 @@ export default {
             'id': doc.id,
             'name': doc.data().name,
             'dept': doc.data().dept,
-            //'position': doc.data().position
+            
           }
           this.users.push(data)
         })
