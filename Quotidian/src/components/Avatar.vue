ea259d1072288add73819f916b8c8de8b1aa2e4d
Shortround <Longleechang@gmail.com>
2021-11-30 17:14:16 -0800

    edit avatar page

--- Quotidian/src/components/Avatar.vue
+++ Quotidian/src/components/Avatar.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="avatar">
     <a v-for="data in Json" :key="data.id" >
-    <img :src="`https://i.ibb.co/${data.source}`" :style="`z-index: ${data.layer};position:absolute`">
+    <img :src="`https://i.ibb.co/${data.source}`" :style="`z-index: ${data.layer}; position:absolute; object-fit: cover; object-position: 20% 10%;`">
     </a>
   </div>
 </template>
