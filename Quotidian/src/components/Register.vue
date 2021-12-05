7e2b01648e0a4635ca373489d41a28759b7d309f
gdub21 <gdubbb21@mail.fresnostate.edu>
2021-12-04 16:43:01 -0800

    fixed bio issue

--- Quotidian/src/components/Register.vue
+++ Quotidian/src/components/Register.vue
@@ -77,7 +77,7 @@ export default {
                      {"id": 6, "name": "hair", "layer": 35, "source": "qJrRKvg/hair4.png"},
                      {"id": 7, "name": "accessory", "layer": 40, "source": "d6C3fhn/acc3.png"}
              ],
-              bio: " ",
+              bio: this.bio
               
             });
             alert(`Account created for ${user.email}`);
