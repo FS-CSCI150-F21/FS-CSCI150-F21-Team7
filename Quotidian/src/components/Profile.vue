0f265ba4479fe56dae638d3d2768d77e893f30a3
Parneet Gill <90004132+Pgill-023@users.noreply.github.com>
2021-12-05 15:44:31 -0800

    Update Profile.vue

--- Quotidian/src/components/Profile.vue
+++ Quotidian/src/components/Profile.vue
@@ -33,12 +33,8 @@
             </div>
         </div>
 
-<<<<<<< HEAD
        <!-- <div class="editprof"><button class="btn-small">Edit bios</button></div> -->
-=======
-        <!-- <div class="editprof"><button class="btn-small">Edit bios</button></div> -->
->>>>>>> da8aa68bdcb96572314d877a4880253bb0acc244
-       
+
     </div>
 </template>
 
